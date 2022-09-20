6bd57af5d0b8e0315689bb381d9986c89c3a516a
Lara <lara.heard50@journalism.cuny.edu>
2022-09-20 19:16:47 -0400

    Final commit (working)

---
+++ main.js
@@ -0,0 +1,16 @@
+const off_switch = document.querySelector(".lights-on img");
+const on_switch = document.querySelector(".lights-off img");
+const body = document.querySelector("body");
+
+off_switch.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    body.classList.add("off");
+
+});
+
+on_switch.addEventListener("click", function (e) {
+        e.preventDefault();
+    
+    body.classList.remove("off");
+});
